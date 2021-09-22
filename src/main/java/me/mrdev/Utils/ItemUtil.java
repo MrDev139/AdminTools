bb2b6d94155e1357ccdd09d513a9ea93a81fb40c
MrDev139 <54265390+MrDev139@users.noreply.github.com>
2021-09-22 21:48:04 +0200

    Made it look less ugly

--- src/main/java/me/mrdev/Utils/ItemUtil.java
+++ src/main/java/me/mrdev/Utils/ItemUtil.java
@@ -7,7 +7,7 @@ import org.bukkit.inventory.meta.ItemMeta;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ItemUtil {
+public class ItemUtil { //dead
 
     private static ItemStack itemStack;
 
